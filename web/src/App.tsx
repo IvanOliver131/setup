40f4b3a87cce88c6accda6ae77b69833e4b99712
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2023-01-26 23:00:35 -0300

    feat: devCommit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,16 +3,9 @@ import { Habit } from "./components/Habit";
 
 import "./styles/global.css";
 
-// fragments
 function App() {
   return (
     <Fragment>
-      <Habit completed={3} />
-      <Habit completed={3} />
-      <Habit completed={3} />
-      <Habit completed={3} />
-      <Habit completed={3} />
-      <Habit completed={3} />
       <Habit completed={3} />
     </Fragment>
   );
