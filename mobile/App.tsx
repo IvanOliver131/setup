d89319c2e08ad657886721c66e6cff22427648fd
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2023-01-27 22:24:49 -0300

    devCommit

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -20,6 +20,7 @@ export default function App() {
     return <Loading />;
   }
 
+  // Inicio do projeto
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
